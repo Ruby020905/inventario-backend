1f417b3e564f5d95c0651b21b1f7fefd1da5269e
Ruby020905 <rubyoforiwa@gmail.com>
2025-09-15 22:03:52 -0500

    eliminar categoria

--- src/main/java/com/company/inventario/controller/CategoryRestController.java
+++ src/main/java/com/company/inventario/controller/CategoryRestController.java
@@ -2,6 +2,7 @@ package com.company.inventario.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,7 +56,7 @@ public class CategoryRestController {
 	}
 	
 	/**
-	 * update categories
+	 * update category
 	 * @param category
 	 * @param id
 	 * @return
@@ -67,5 +68,16 @@ public class CategoryRestController {
 		return response;
 	}
 	
+	/**
+	 * delete category
+	 * @param id
+	 * @return
+	 */
+	@DeleteMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> deleteById( @PathVariable Long id) {
+
+		ResponseEntity<CategoryResponseRest> response = service.deleteById(id);
+		return response;
+	}
 	
 }
