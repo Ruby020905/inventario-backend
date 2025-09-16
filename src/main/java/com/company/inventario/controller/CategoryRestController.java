f8b14a52e54bffef35fe6ac4a091a9159a8cee59
Ruby020905 <rubyoforiwa@gmail.com>
2025-09-15 22:08:34 -0500

    cross origin para el servidor de angular

--- src/main/java/com/company/inventario/controller/CategoryRestController.java
+++ src/main/java/com/company/inventario/controller/CategoryRestController.java
@@ -2,6 +2,7 @@ package com.company.inventario.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.company.inventario.model.Category;
 import com.company.inventario.response.CategoryResponseRest;
 import com.company.inventario.services.ICategoryService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryRestController {
