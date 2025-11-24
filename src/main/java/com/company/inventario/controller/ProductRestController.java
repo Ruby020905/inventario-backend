ac3c5c04454f8fd8aac6a121c47ef796e0fe8e08
Ruby020905 <rubyoforiwa@gmail.com>
2025-11-23 19:15:03 -0500

    buscar producto por id

--- src/main/java/com/company/inventario/controller/ProductRestController.java
+++ src/main/java/com/company/inventario/controller/ProductRestController.java
@@ -5,6 +5,8 @@ import java.util.Date;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -53,6 +55,12 @@ public class ProductRestController {
 				ResponseEntity<ProductResponseRest> response = productService.save(product, categoryID);
 				return response;
 		
+	}
+	
+	@GetMapping("/products/{id}")
+	public ResponseEntity<ProductResponseRest> searchById(@PathVariable Long id){
+		ResponseEntity<ProductResponseRest> response = productService.searchById(id);
+		return response;
 	}
 	
 }
--- src/main/java/com/company/inventario/services/IProductService.java
+++ src/main/java/com/company/inventario/services/IProductService.java
@@ -9,4 +9,5 @@ import com.company.inventario.response.ProductResponseRest;
 public interface IProductService {
 
 	public ResponseEntity<ProductResponseRest> save(Product product, Long categoryId);
+	public ResponseEntity<ProductResponseRest> searchById(Long id);
 }
